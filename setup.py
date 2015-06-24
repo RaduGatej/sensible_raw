3cd3d2ea9b46e0358272d1cb650af5fe494af168
radugatej <radu.gatej@gmail.com>
2015-06-25 01:42:16 +0200

    Python egg setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sensible_raw',
+      version='0.1',
+      description='Raw data management for sensibledtu project',
+      author='Radu Gatej',
+      author_email='radu.gatej@gmail.com',
+      license='MIT',
+      packages=['sensible_loader'],
+      zip_safe=False)
