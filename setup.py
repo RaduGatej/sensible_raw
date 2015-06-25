b57d7958145d0e65c59e9870dc6a5e6822c68961
radugatej <radu.gatej@gmail.com>
2015-06-25 02:19:41 +0200

    Fixed setup to include sub packages

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+import os
 from setuptools import setup
+
+pjoin = os.path.join
+
+packages = []
+for d, _, _ in os.walk('sensible_raw'):
+    if os.path.exists(pjoin(d, '__init__.py')):
+        packages.append(d.replace(os.path.sep, '.'))
 
 setup(name='sensible_raw',
       version='0.1',
+      packages = packages,
       description='Raw data management for sensibledtu project',
       author='Radu Gatej',
       author_email='radu.gatej@gmail.com',
