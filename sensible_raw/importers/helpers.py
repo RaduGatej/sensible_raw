fc05d3d5517c5111b881e6dad63b1e5f7f7a2f0c
radugatej <radu.gatej@gmail.com>
2015-08-14 14:40:25 +0200

    Added functionality for importing data

---
+++ sensible_raw/importers/helpers.py
@@ -0,0 +1,149 @@
+from collections import defaultdict
+import json
+import logging
+import os
+
+import MySQLdb
+import MySQLdb.cursors
+from pymongo import MongoClient
+from sqlalchemy import *
+from sqlalchemy.sql import select
+import time
+import decimal
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+
+class MySQLHelper(object):
+	def __init__(self, config):
+		self.hostname = config["hostname"]
+		self.user = config["user"]
+		self.password = config["password"]
+		self.database = config["database"]
+		self.table_name = config["table"]
+		self.query_fields = config["query_fields"]
+
+	def __get_connection(self):
+		connection_string = 'mysql://%s:%s@%s/%s' % (self.user, self.password, self.hostname, self.database)
+		engine = create_engine(connection_string, connect_args={'cursorclass': MySQLdb.cursors.SSCursor})
+		connection = engine.connect()
+		return connection
+
+	def query_database(self, process_row_function, after=None):
+		connection = self.__get_connection()
+		metadata = MetaData(bind=connection.engine)
+		table = Table(self.table_name, metadata, autoload=True)
+		query = select([getattr(table.c, field) for field in self.query_fields])
+		if after:
+			field, value = after
+			query = query.where(getattr(table.c, field) > value)
+		result = connection.execute(query)
+		for row in result:
+			row_dict = {}
+			for index, item in enumerate(list(row)):
+				if isinstance(item, decimal.Decimal):
+					item = float(item)
+				row_dict[self.query_fields[index]] = item
+			process_row_function(row_dict)
+
+
+class SensibleMongoHelper(object):
+	INSERT_BATCH_SIZE = 100000
+
+	def __init__(self, config):
+		self.client = MongoClient()
+		self.client.admin.authenticate(config["user"], config["password"])
+		self.db = self.client[config["database"]]
+		self.insert_batch = defaultdict(list)
+		self.collection_name = config["table"]
+
+	def insert_row(self, row):
+		self.insert_batch[self.collection_name].append(row)
+		if len(self.insert_batch[self.collection_name]) == self.INSERT_BATCH_SIZE:
+			self.db[self.collection_name].insert(self.insert_batch[self.collection_name])
+			logging.warning("Inserted %s in mongo", len(self.insert_batch[self.collection_name]))
+			self.insert_batch[self.collection_name] = []
+
+	def commit_changes(self):
+		for collection, batch in self.insert_batch.items():
+			self.db[collection].insert(batch)
+			logging.warning("Inserted %s in mongo", len(batch))
+		self.insert_batch = defaultdict(list)
+
+	def query_database(self):
+		return [doc for doc in self.db[self.collection_name].find()]
+
+
+class FieldIndexerHelper():
+	INDEX_FOLDER = "indices"
+
+	def __init__(self, fields_to_index):
+		self.field_indices = defaultdict(lambda: defaultdict(self.__integer_field_index))
+		self.__load_indices()
+		self.index_counters = defaultdict(int)
+		self.__load_index_counters()
+		self.fields_to_index = fields_to_index
+		self.current_field = None
+
+	def __load_index_counters(self):
+		for index_name, indices in self.field_indices.items():
+			self.index_counters[index_name] = max(indices.values())
+
+	def __load_indices(self):
+		for filename in os.listdir(self.INDEX_FOLDER):
+			index = json.loads(open(self.INDEX_FOLDER + "/" + filename, "r").read())
+			index_name = filename.split(".")[0]
+			self.field_indices[index_name] = defaultdict(self.__integer_field_index, index)
+
+	def index_fields(self, row):
+
+		for field, index_name in self.fields_to_index:
+			value_to_index = row[field]
+			if not isinstance(value_to_index, basestring):
+				continue
+			self.current_field = field
+			row[field] = self.field_indices[index_name][value_to_index]
+
+		return row
+
+	def save_indexes(self):
+		for index_name, indices in self.field_indices.items():
+			print indices
+			f = open(self.INDEX_FOLDER + "/" + index_name + ".json", "w")
+			f.write(json.dumps(dict(indices)))
+			f.close()
+
+	def __integer_field_index(self):
+		self.index_counters[self.current_field] += 1
+		return self.index_counters[self.current_field]
+
+
+class BluetoothMacMapper(object):
+	def __init__(self):
+		self.device_inventory = json.loads(open("device_inventory", "r").read())
+
+	def map_bt_mac_to_user(self, bt_mac, timestamp):
+		inventory_entries = self.device_inventory.get(bt_mac)
+		if not inventory_entries:
+			return -1
+
+		for entry in inventory_entries:
+			if entry['start'] <= int(time.mktime(timestamp.timetuple())) <= entry['end']:
+				return entry['user']
+
+		return -1
+
+	def map(self, row):
+		row["bt_mac"] = self.map_bt_mac_to_user(row["bt_mac"], row["timestamp"])
+		return row
+
+
+class PhoneNumberMapper(object):
+	def __init__(self):
+		self.phone_book = json.loads(open("phone_book", "r").read())
+
+	def map(self, row):
+		if not self.phone_book.get(row['number']):
+			return row
+		row["number"] = self.phone_book.get(row["number"])
+		return row
---
+++ sensible_raw/importers/importers.py
@@ -0,0 +1,49 @@
+import helpers
+
+
+class Importer(object):
+	def __init__(self, config):
+		self.remote_db = helpers.MySQLHelper(config["source_db"])
+		self.local_db = helpers.SensibleMongoHelper(config["target_db"])
+		self.indexer = helpers.FieldIndexerHelper(config["fields_to_index"])
+		try:
+			self.mapper = getattr(helpers, config["mapper"])()
+		except:
+			self.mapper = None
+		self.after = None
+
+	def process_row(self, row):
+		if self.mapper:
+			row = self.mapper.map(row)
+		self.local_db.insert_row(self.indexer.index_fields(row))
+
+	def import_data(self):
+		self.remote_db.query_database(self.process_row, after=self.after)
+		self.indexer.save_indexes()
+		self.local_db.commit_changes()
+
+
+class SensibleDataImporter(Importer):
+	def __init__(self, config):
+		Importer.__init__(self, config)
+		self.last_id_db = helpers.SensibleMongoHelper(config["target_db"])
+		self.current_id = self.get_last_id()
+		self.after = ("id", self.current_id)
+
+	def process_row(self, row):
+		self.local_db.collection_name = row["timestamp"].strftime("%B_%Y").lower()
+		if row["id"] > self.current_id:
+			self.current_id = row["id"]
+		Importer.process_row(self, row)
+
+	def import_data(self):
+		Importer.import_data(self)
+		self.last_id_db.insert_row({"scan_id": self.current_id})
+		self.last_id_db.commit_changes()
+
+	def get_last_id(self):
+		self.last_id_db.collection_name = "last_scan_ids"
+		last_ids = self.last_id_db.query_database()
+		if not last_ids:
+			return 0
+		return max(last_ids, key=lambda x: ["scan_id"])["scan_id"]
